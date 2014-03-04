304a3701d21011a44e68c4686480894a1d9cc6d7
edeustace <ed.eustace@gmail.com>
2014-03-04 13:11:22 +0000

    use init code

---
+++ init.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('.summernote').summernote({
+    height: 300,                  // set editable area's height
+    focus: true,                  // set focus editable area after summernote loaded
+    tabsize: 2,                   // size of tab
+    // disableDragAndDrop: false, // disable drag and drop event
+    codemirror: {                 // code mirror options
+      theme: 'monokai'
+    },
+    buttons: [
+      {
+        onClick: function (editor, $editable, range) {
+          editor.restoreRange($editable);
+          var url = 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQsUU2rnO7aifTmF-TsnYfEN7HsAXLgYu_iUjzBS6aj1WFh6oPF';
+          var $node = $('<img>').attr('src', url);
+          range.create().insertNode($node[0]);
+        },
+        uid: 'add-kenny-powers',
+        toolbarButton: function () {
+          return [
+            '<button type="button" ',
+            '  class="btn btn-default btn-sm btn-small" ',
+            '  title="test" data-event="' + this.uid + '" ',
+            '  tabindex="-1">',
+            '  <i class="fa fa-trophy"></i>',
+            '</button>'
+          ].join('\n');
+        }
+      }
+    ]
+  });
+});
